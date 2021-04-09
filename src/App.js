9c0efb42ced451f906e4092cb4f1520cd15115a1
Rigo Gonzalez <rigognzlz@gmail.com>
2021-04-09 18:06:42 +0800

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,14 +1,90 @@
 import {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import axios from 'axios';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {Container, Row, Col, Button, Form} from 'react-bootstrap';
-import CatList from './components/CatList';
+import {Container, Row, Col, Card, Button, Form} from 'react-bootstrap';
+
 import CatDetails from './components/CatDetails';
 
 function App() {
+	const [breedsLoaded, setBreedsLoaded] = useState(0);
+	const [breeds, setBreeds] = useState([]);
+	const [selectedBreed, setSelectedBreed] = useState([]);
+
+	const [page, setPage] = useState(1);
+
+	useEffect(()=>{
+		const getBreeds = () => {
+
+			axios({
+				method: 'get',
+				url: "https://api.thecatapi.com/v1/breeds/",
+				headers: {
+					'x-api-key': '5de2bb0e-5138-4371-ad0d-4431aba43c4d'
+				},
+			}).then((response)=>{
+				console.log("get cat breeds", response);
+				setBreeds(response.data);
+				setBreedsLoaded(1);
+			},(error)=>{
+				console.log("error getting cat breeds", error);
+			})
+		}
+
+		getBreeds();
+
+	}, [])
+
+	const changeSelectedBreed = (newBreed) =>{
+		console.log("New selected", newBreed.target.value);
+		if(newBreed.target.value !== 0){
+
+			// setSelectedBreed(newBreed.target.value)
+			axios({
+				method: 'get',
+				url: "https://api.thecatapi.com/v1/images/search",
+				headers: {
+					'x-api-key': '5de2bb0e-5138-4371-ad0d-4431aba43c4d'
+				},
+				params: {
+				    'breed_id': newBreed.target.value,
+					'page': page,
+					'limit': 10
+				}
+			}).then((response)=>{
+				console.log("get cat breed img", response);
+				setSelectedBreed(response.data);
+			},(error)=>{
+				console.log("error getting cat breed imgs", error);
+			})
+		}
+
+	}
+
+	const loadMore = () =>{
+		setPage(page + 1);
+		axios({
+			method: 'get',
+			url: "https://api.thecatapi.com/v1/images/search",
+			headers: {
+				'x-api-key': '5de2bb0e-5138-4371-ad0d-4431aba43c4d'
+			},
+			params: {
+			    'breed_id': selectedBreed[0].breeds[0].id,
+				'page': page,
+				'limit': 10
+			}
+		}).then((response)=>{
+			console.log("loading more cat breed img", response);
+			console.log(selectedBreed, response.data)
+			setSelectedBreed([...selectedBreed, ...response.data]);
+		},(error)=>{
+			console.log("error loading more cat breed imgs", error);
+		})
+	}
 
   return (
 	<Router>
@@ -28,28 +104,39 @@ function App() {
 						  <section>
 						  	<Form>
 								<Form.Label>Breed</Form.Label>
-								<Form.Control as="select">
-									<option>1</option>
-									<option>2</option>
+								<Form.Control as="select" onChange={changeSelectedBreed} disabled={!breedsLoaded}>
+								<option value="0">Select Cat Breed</option>
+								{breeds.map((breed)=>{
+									return <option value={breed.id} key={breed.id}>{breed.name}</option>
+								})}
 								</Form.Control>
 							</Form>
 						  </section>
 						</Col>
 					</Row>
 
-
-					<CatList />
+					<div className="cat-list">
+						<Row>
+								{selectedBreed.map((breed, id)=>{
+									return <Col md="3"><Card key={breed.id}>
+										<Card.Img variant="top" src={breed.url} />
+										<Card.Text>{breed.name}</Card.Text>
+										<Button variant="primary" color="white"><Link to={breed.id}>View Details</Link></Button>
+										</Card></Col>
+								})}
+						</Row>
+					</div>
 
 					<Row>
 			  			<Col>
-							<Button variant="success" size="sm">Load More</Button>
+							<Button variant="success" size="sm" onClick={loadMore} disabled={!breedsLoaded || selectedBreed.length === 0}>Load More</Button>
 						</Col>
 					</Row>
 				</Container>
 		    </div>
 		)} />
 
-		<Route path='/details' component={CatDetails} />
+		<Route path='/:breed_id' component={CatDetails} />
 
 	</Router>
   );
--- src/components/CatDetails.js
+++ src/components/CatDetails.js
@@ -1,10 +1,41 @@
-import { Link } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import { Link, useParams } from 'react-router-dom';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import axios from 'axios';
+
 
 import {Container, Row, Col, Card, Button} from 'react-bootstrap';
 
 function CatDetails() {
+
+
+	const [cat, setCat] = useState([]);
+	const location = useParams();
+	useEffect(()=>{
+
+		const getCat = () => {
+			axios({
+				method: 'get',
+				url: "https://api.thecatapi.com/v1/images/" + location.breed_id,
+				headers: {
+					'x-api-key': '5de2bb0e-5138-4371-ad0d-4431aba43c4d'
+				},
+			}).then((response)=>{
+				console.log("get cat", response);
+				setCat(response.data);
+			},(error)=>{
+				console.log("error getting cat", error);
+			})
+		}
+
+		getCat();
+
+	}, [location.breed_id])
+
+
+
   return (
     <div>
 		<Container>
@@ -12,11 +43,19 @@ function CatDetails() {
 				<Col>
 					<Card>
 						<Card.Header>
-							<Button variant="default"><Link to='/'>Back</Link></Button>
+							<Button variant="primary"><Link to='/'>Back</Link></Button>
 						</Card.Header>
+						<Card.Img src={cat.url} />
 						<Card.Body>
-							<Card.Title>Cat Name</Card.Title>
-							<Card.Text>Detail of Cat</Card.Text>
+							<Card.Title>
+								<h2>{cat.breeds && cat.breeds[0].name}</h2>
+								<h4>Origin: {cat.breeds && cat.breeds[0].origin}</h4>
+								<strong>{cat.breeds && cat.breeds[0].temperament}</strong>
+							</Card.Title>
+							<Card.Text>
+
+								{cat.breeds && cat.breeds[0].description}
+							</Card.Text>
 						</Card.Body>
 					</Card>
 				</Col>
